54eecf4a915251aa76f098228656f7f0165d7bd5
Yukari Chiba <i@0x7f.cc>
2023-01-13 16:08:53 +0800

    Update sync.sh

--- repo-sync/sync.sh
+++ repo-sync/sync.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
 
-SYNC_HOST=192.168.1.150
+RHOST=obs_server
 
 BASE_DIR=/mirror/data/eweos
 REPOS=(
         Main
         Testing
 )
+ARCHS=(
+        x86_64
+        aarch64
+)
 
 for REPO_NAMEX in ${REPOS[@]}; do
         REPO_NAME=${REPO_NAMEX,,}
-                
-        echo "Syncing repo ${REPO_NAME} ..."
-
         mkdir -p ${BASE_DIR}/${REPO_NAME}/os/${REPO_ARCH}
 
-        if rsync --list-only \
-                rsync://$SYNC_HOST/Repo/$REPO_NAMEX/Rolling/x86_64/ \
-                | grep ".db$" \
-                | grep -q "^-"; then
+        for REPO_ARCH in ${ARCHS[@]}; do
 
-        rsync -atrzlu --delete-after \
-                rsync://$SYNC_HOST/Repo/$REPO_NAMEX/Rolling/ \
-                $BASE_DIR/$REPO_NAME/os/
+                echo "Syncing repo ${REPO_NAME} ${REPO_ARCH}"
 
-        for f in ${BASE_DIR}/${REPO_NAME}/os/*/eweOS_${REPO_NAMEX}_Rolling*; do
-                fn=$(echo $f | sed "s/eweOS_${REPO_NAMEX}_Rolling/${REPO_NAME}/g")
-                mv "$f" "$fn"
+                if rsync --list-only \
+                        rsync://$RHOST/Repo/$REPO_NAMEX/Rolling/$REPO_ARCH/ \
+                                | grep ".db$" \
+                                | grep -q "^-"; then
+
+                        rsync -atrzlu --delete-after \
+                                rsync://$RHOST/Repo/$REPO_NAMEX/Rolling/$REPO_ARCH/ \
+                                        $BASE_DIR/$REPO_NAME/os/$REPO_ARCH/
+
+                        for f in ${BASE_DIR}/${REPO_NAME}/os/$REPO_ARCH/eweOS_${REPO_NAMEX}_Rolling*; do
+                                fn=$(echo $f | sed "s/eweOS_${REPO_NAMEX}_Rolling/${REPO_NAME}/g")
+                                mv "$f" "$fn"
+                        done
+                else
+                        echo "${REPO_NAME} ${REPO_ARCH} db file error, skipped."
+                fi
         done
-
-        else
-                echo "${REPO_NAME} db file error, skipped."
-        fi
 done
 
 cat <<EOF >$BASE_DIR/pacman.conf
@@ -54,7 +59,12 @@ Server = http://os-repo.ewe.moe/eweos/$REPO_NAME/os/\$arch/
 EOF
 done
 
-echo "Syncing images ..."
+echo "Syncing images for x86_64 ..."
 rsync -atrzlu --delete-after \
-        rsync://$SYNC_HOST/Repo/Image/Arch/ \
-        $BASE_DIR-images/
+        rsync://$RHOST/Repo/Image/Arch/ \
+        $BASE_DIR-images/x86_64
+
+echo "Syncing images for aarch64 ..."
+rsync -atrzlu --delete-after \
+        rsync://$RHOST/Repo/Image/ArchArm/ \
+        $BASE_DIR-images/aarch64
