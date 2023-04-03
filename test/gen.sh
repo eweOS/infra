2fe5813bc49944794c4a9232f779eeeedc67145b
YukariChiba <i@0x7f.cc>
2023-04-04 05:15:54 +0800

    Add pkg update list page

--- test/gen.sh
+++ test/gen.sh
@@ -36,3 +36,24 @@ Updated: `date`
 EOF
 
 cp /tmp/www-info.html /var/ewe/index.html
+
+cat <<EOF > /tmp/upd-info.html
+<!DOCTYPE html>
+<html>
+<head>
+<title>eweOS Test Server | Software Update</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="generator" content="http://chalarangelo.github.io/htmltemplategenerator/">
+</head>
+<body>
+<h1>eweOS Software Update</h1>
+<hr />
+<pre>
+`archversion report -n`
+</pre>
+</body>
+</html>
+EOF
+
+cp /tmp/upd-info.html /var/ewe/update.html
