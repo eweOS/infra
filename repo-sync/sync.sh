beaf431486ecb71524fad6c7de427eb84e75058f
Yukari Chiba <i@0x7f.cc>
2024-04-17 19:08:59 +0800

    Update sync.sh

--- repo-sync/sync.sh
+++ repo-sync/sync.sh
@@ -10,10 +10,10 @@ REPOS=(
 ARCHS=(
         x86_64
         aarch64
+        riscv64
 )
 
-for REPO_NAMEX in ${REPOS[@]}; do
-        REPO_NAME=${REPO_NAMEX,,}
+for REPO_NAME in ${REPOS[@]}; do
         mkdir -p ${BASE_DIR}/${REPO_NAME}/os/${REPO_ARCH}
 
         for REPO_ARCH in ${ARCHS[@]}; do
@@ -21,16 +21,16 @@ for REPO_NAMEX in ${REPOS[@]}; do
                 echo "Syncing repo ${REPO_NAME} ${REPO_ARCH}"
 
                 if rsync --list-only \
-                        rsync://$RHOST/Repo/$REPO_NAMEX/Rolling/$REPO_ARCH/ \
-                                | grep ".db$" \
+                        rsync://$RHOST/repo-$REPO_NAME/$REPO_ARCH/ \
+                                | grep ".db.tar.gz$" \
                                 | grep -q "^-"; then
 
                         rsync -atrzlu --delete-after \
-                                rsync://$RHOST/Repo/$REPO_NAMEX/Rolling/$REPO_ARCH/ \
+                                rsync://$RHOST/repo-$REPO_NAME/$REPO_ARCH/ \
                                         $BASE_DIR/$REPO_NAME/os/$REPO_ARCH/
 
-                        for f in ${BASE_DIR}/${REPO_NAME}/os/$REPO_ARCH/eweOS_${REPO_NAMEX}_Rolling*; do
-                                fn=$(echo $f | sed "s/eweOS_${REPO_NAMEX}_Rolling/${REPO_NAME}/g")
+                        for f in ${BASE_DIR}/${REPO_NAME}/os/$REPO_ARCH/eweOS_${REPO_NAME^}_rolling*; do
+                                fn=$(echo $f | sed "s/eweOS_${REPO_NAME^}_rolling/${REPO_NAME}/g")
                                 mv "$f" "$fn"
                         done
                 else
@@ -38,33 +38,3 @@ for REPO_NAMEX in ${REPOS[@]}; do
                 fi
         done
 done
-
-cat <<EOF >$BASE_DIR/pacman.conf
-[options]
-HoldPkg     = pacman musl busybox
-Architecture = auto
-Color
-CheckSpace
-ParallelDownloads = 8
-
-EOF
-
-for REPO_NAMEX in ${REPOS[@]}; do
-REPO_NAME=${REPO_NAMEX,,}
-cat <<EOF >>$BASE_DIR/pacman.conf
-[$REPO_NAME]
-SigLevel = Never
-Server = http://os-repo.ewe.moe/eweos/$REPO_NAME/os/\$arch/
-
-EOF
-done
-
-echo "Syncing images for x86_64 ..."
-rsync -atrzlu --delete-after \
-        rsync://$RHOST/Image/Arch/x86_64/:repo/ \
-        $BASE_DIR-images/x86_64
-
-echo "Syncing images for aarch64 ..."
-rsync -atrzlu --delete-after \
-        rsync://$RHOST/Image/ArchArm/aarch64/:repo/ \
-        $BASE_DIR-images/aarch64
