9b3cebe62352e12495abe8a4e9414c44e11b8ebd
Yukari Chiba <i@0x7f.cc>
2024-05-09 11:56:45 +0800

    Update pkg.cgi.sh

--- test/pkg.cgi.sh
+++ test/pkg.cgi.sh
@@ -1,6 +1,24 @@
 #!/bin/env bash
 
+EXTCMD=${REQUEST_URI#*\?}
+REQUEST_URI=${REQUEST_URI%\?*}
 PKGNAME=${REQUEST_URI#/package/}
+
+case EXTCMD in
+  "dep")
+    EXTCMD="| grep '^Depends On' | cut -f 2 -d ':'"
+    ;;
+  "rdep")
+    EXTCMD="| grep '^Required By' | cut -f 2 -d ':'"
+    ;;
+  "ver")
+    EXTCMD="| grep '^Version' | cut -f 2 -d ':'"
+    ;;
+  *)
+    EXTCMD="| cat"
+    ;;
+esac
+
 
 if [ -z "${PKGNAME}" ]; then
 cat << EOF
@@ -16,7 +34,7 @@ if [[ $HTTP_USER_AGENT == *"curl"* ]]; then
 cat << EOF
 Content-type: text/plain
 
-`pacman -Sii $PKGNAME 2>&1`
+`pacman -Sii $PKGNAME 2>&1 | $EXTCMD`
 EOF
 
 else
@@ -27,7 +45,7 @@ Content-type: text/html
 <html>
 <head><title>Query for $PKGNAME</title></head>
 <body>
-<pre>`pacman -Sii $PKGNAME 2>&1`</pre>
+<pre>`pacman -Sii $PKGNAME 2>&1 | $EXTCMD`</pre>
 </body>
 </html>
 EOF
