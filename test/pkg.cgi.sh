9db86b82d4085440a50669b56fe0a068d457436c
Yukari Chiba <i@0x7f.cc>
2024-05-09 12:22:28 +0800

    Update pkg.cgi.sh

--- test/pkg.cgi.sh
+++ test/pkg.cgi.sh
@@ -4,26 +4,6 @@ EXTCMD=${REQUEST_URI#*\?}
 REQUEST_URI=${REQUEST_URI%\?*}
 PKGNAME=${REQUEST_URI#/package/}
 
-case $EXTCMD in
-  "dep")
-    EXTCMD="grep '^Depends On'"
-    EXT2CMD="cut -f 2 -d ':'"
-    ;;
-  "rdep")
-    EXTCMD="grep '^Required By'"
-    EXT2CMD="cut -f 2 -d ':'"
-    ;;
-  "ver")
-    EXTCMD="grep '^Version'"
-    EXT2CMD="cut -f 2 -d ':'"
-    ;;
-  *)
-    EXTCMD="cat"
-    EXT2CMD="cat"
-    ;;
-esac
-
-
 if [ -z "${PKGNAME}" ]; then
 cat << EOF
 Content-type: text/plain
@@ -33,12 +13,28 @@ EOF
 exit
 fi
 
+case $EXTCMD in
+  "dep")
+    GREPSTR='^Depends On'
+    ;;
+  "rdep")
+    GREPSTR='^Required By'
+    ;;
+  "ver")
+    GREPSTR='^Version'
+    ;;
+esac
+
+if [ ! -z $GREPSTR ]; then
+  OUTPUT=`pacman -Sii $PKGNAME 2>&1 | grep "$GREPSTR" | cut -f 2 -d ':'`
+fi
+
 if [[ $HTTP_USER_AGENT == *"curl"* ]]; then
 
 cat << EOF
 Content-type: text/plain
 
-`pacman -Sii $PKGNAME 2>&1 | $EXTCMD | $EXT2CMD`
+`( test $OUTPUT && echo $OUTPUT) || (pacman -Sii $PKGNAME 2>&1 )`
 EOF
 
 else
@@ -49,7 +45,9 @@ Content-type: text/html
 <html>
 <head><title>Query for $PKGNAME</title></head>
 <body>
-<pre>`pacman -Sii $PKGNAME 2>&1 | $EXTCMD | $EXT2CMD`</pre>
+<pre>
+`( test $OUTPUT && echo $OUTPUT) || (pacman -Sii $PKGNAME 2>&1 )`
+</pre>
 </body>
 </html>
 EOF
